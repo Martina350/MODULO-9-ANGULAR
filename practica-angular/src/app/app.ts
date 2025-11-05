6cefc8a61fd0bbd4d12c5c7d48ec4aa1a906681a
Martina <martinacamilaleon@gmail.com>
2025-11-05 12:28:16 -0500

    Components Example

--- practica-angular/src/app/app.ts
+++ practica-angular/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProductComponent} from './product/product';
+import {ProfileComponent} from './profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductComponent, ProfileComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('coder');
---
+++ practica-angular/src/app/product/product.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-product",
+  templateUrl: "./product.html",
+  styleUrls: ["./product.css"]
+})
+
+export class ProductComponent {
+  // Component logic goes here
+}
