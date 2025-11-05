4c4091923eb483b90c3d8dc16d1a2c9ce1a42f96
Martina <martinacamilaleon@gmail.com>
2025-11-05 11:08:25 -0500

    Modified app.html

--- practica-angular/src/app/app.ts
+++ practica-angular/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('camzlfm');
+  protected readonly title = signal('coder');
 }
