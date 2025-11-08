3d0a4ec9dff87623d28f46c4101581e1e500b200
Martina <martinacamilaleon@gmail.com>
2025-11-07 21:06:06 -0500

    Example Docheck

--- practica-angular/src/app/profile/profile.ts
+++ practica-angular/src/app/profile/profile.ts
@@ -11,21 +11,31 @@ import { Loans } from "../loans/loans";
 })
 
 export class ProfileComponent {
-  
+
   public name: string;
   public age: number;
   public location: string;
   public occupation: string;
   public phone: string;
 
-  
+  ngDoCheck() {
+    console.log("ProfileComponent updated");
+  }
   constructor() {
     this.name = "Martina Leon";
     this.age = 19;
     this.location = "South Park - Colorado";
     this.occupation = "Software Developer";
     this.phone = "(+593) 96-283-7277";
-    
+
     console.log("Se cargo el ProfileComponent correctamente");
+  }
+
+  changeAge() {
+    this.age = 20;
+  }
+
+  changeLocation() {
+    this.location = "Springfield - The Simpsons";
   }
 }
